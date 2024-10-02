82a47df5079a5e2b08c480c11d521beaa41769ba
PKumar0358 <pramodkumar40358@gmail.com>
2024-10-02 22:15:33 +0530

    .........

---
+++ Assets/MyPhotonLib/Runtime/Scripts/PhotonEventWrapper.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PRK_PhotonLib
+{
+    public class PhotonEventWrapper 
+    {
+        private static Dictionary<byte, string> dataKeys = new Dictionary<byte, string>()
+        {
+            {0,"" },
+            {1,"" },
+            {2,"" },
+            {3,"" },
+            {4,"" },
+            {5,"" },
+        };
+        public PhotonEventWrapper()
+        {
+
+        }
+
+        public void Add(string ke,object value_)
+        {
+
+        }
+    }
+}
